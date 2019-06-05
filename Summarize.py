3cd7bb00beb55525e1657d6399b6ecb8f0b423ab
Eric Jones <ejon@broadinstitute.org>
2019-06-05 09:34:21 -0400

    Added a bunch of data and also the ability to write an output tsv. Script now depends on Gordon's copy of CancerGenomeAnalysis repository and takes two parameters

--- Summarize.py
+++ Summarize.py
@@ -1,9 +1,12 @@
 #!/bin/env python
+#
 import os
 import csv
 from collections import Counter
 import sys
 import re
+sys.path.append("/xchip/cga_home/gsaksena/svn/CancerGenomeAnalysis/trunk/Python/util/")
+import cga_util
 #
 # program to take as input a set of file data produced by a file system scanner and produce a report containing
 # for each file owner
@@ -14,18 +17,20 @@ import re
 #  5. latest modified date
 #  6. count of files not readable by "others" or group
 
-def summarize_disk_usage(infile):
+def summarize_disk_usage(infile,outfile):
     binList = [1000000,1000000000,10000000000]
     extList = [".csv",".bam","gz","tar"]
     nBins = len(binList)
-    with open(infile, 'r') as csvfile:
+    with open(infile, 'rU') as csvfile:
         reader = csv.DictReader(f=csvfile,dialect='excel-tab',lineterminator= '\n',quoting=csv.QUOTE_MINIMAL)
         info_by_user = dict()
-	for row in reader:
+        try:
+	  for row in reader:
             user = row['username']
 	    size = int(row['size'])
 	    if user not in info_by_user:
 	        info_by_user[user] = Counter()
+		info_by_user[user]["user"] = user
 	    info_by_user[user]["fileCnt"] += 1
 	    info_by_user[user]["fileSize"] += size
 	    binNo=1
@@ -44,11 +49,16 @@ def summarize_disk_usage(infile):
 		if re.search(regEx,row['filepath'],flags=re.IGNORECASE):
 		  info_by_user[user]["fileType"+ext] += 1
 		  info_by_user[user]["fileTypeSize"+ext] += size
+        except:
+	  print(row['filepath'])
 
-    print(info_by_user)
-
+    #print(info_by_user)
+    
+    fieldnames = ('user','fileCnt',"fileSize","fileCntBin1","fileCntBin2","fileCntBin3","fileCntBin4","fileSizeBin1","fileSizeBin2","fileSizeBin3","fileSizeBin4","fileType.csv","fileType.bam","fileTypegz","fileTypetar","fileTypeSize.csv","fileTypeSize.bam","fileTypeSizegz","fileTypeSizetar")
+    cga_util.dump_dict_table(outfile,info_by_user,fields=fieldnames,ragged_ok = True)
     
 
 if __name__ == '__main__':
     scanFile = sys.argv[1]
-    summarize_disk_usage(infile=scanFile)
+    sumFile = sys.argv[2]
+    summarize_disk_usage(infile=scanFile,outfile=sumFile)
