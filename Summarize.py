eec15c2b1df94ba6f9ac4d9c04f847eda9e2390b
Eric Jones <ejon@broadinstitute.org>
2019-06-12 15:57:57 -0400

    Additional changes to summarize file sizes by TB

--- Summarize.py
+++ Summarize.py
@@ -16,14 +16,21 @@ import cga_util
 #  5. latest modified date
 #  6. count of files not readable by "others" or group
 
+def TB_from_bytes(bytes):
+    TB = '{size:.2f}'.format(size=bytes/1.1e12)
+    return TB
+
+
 def summarize_disk_usage(infile,outfile):
     binList = [1e6,1e9,1e10,1e11,1e12,1e21]
     binNameList = ["<1 MB","<1 GB","< 10GB","< 100GB","< 1TB","> 1TB"]
     cntBinNameList = ["Cnt " + s for s in binNameList]
     sizeBinNameList = ["Size " + s for s in binNameList]
+    TBsizeBinNameList = ["Size " + s + " (TB)" for s in binNameList]
     extList = ["csv","bam","gz","tar"]
     cntExtList = ["Cnt " + s for s in extList]
     sizeExtList = ["Size " + s for s in extList]
+    TBsizeExtList = ["Size " + s + " (TB)" for s in extList]
     nBins = len(binList)
     nExts = len(extList)
     with open(infile, 'rU') as csvfile:
@@ -32,34 +39,44 @@ def summarize_disk_usage(infile,outfile):
 #        try:
         for row in reader:
             user = row['username']
-	    size = int(row['size'])
-	    if user not in info_by_user:
-	        info_by_user[user] = Counter()
-		info_by_user[user]["user"] = user
-	    info_by_user[user]["fileCnt"] += 1
-	    info_by_user[user]["fileSize"] += size
-	    if info_by_user[user]["Last access"] < row["last_access"]:
-	        info_by_user[user]["Last access"] = row["last_access"]
-	    if info_by_user[user]["Last modified"] < row["last_modified"]:
-	        info_by_user[user]["Last modified"] = row["last_modified"]
-	    for index, binTop in enumerate(binList,start=0):
-	        if size <= binTop:
-	            info_by_user[user][cntBinNameList[index]] += 1
-	            info_by_user[user][sizeBinNameList[index]] += size
-		    break
-	    for idx, ext in enumerate(extList,start=0):
-		if row['filepath'].lower().endswith(ext):
-		  info_by_user[user][cntExtList[idx]] += 1
-		  info_by_user[user][sizeExtList[idx]] += size
+            size = int(row['size'])
+            if user not in info_by_user:
+                info_by_user[user] = Counter()
+                info_by_user[user]["user"] = user
+            info_by_user[user]["fileCnt"] += 1
+            info_by_user[user]["fileSize"] += size
+            if info_by_user[user]["Last access"] < row["last_access"]:
+                info_by_user[user]["Last access"] = row["last_access"]
+            if info_by_user[user]["Last modified"] < row["last_modified"]:
+                info_by_user[user]["Last modified"] = row["last_modified"]
+            for index, binTop in enumerate(binList,start=0):
+                if size <= binTop:
+                    info_by_user[user][cntBinNameList[index]] += 1
+                    info_by_user[user][sizeBinNameList[index]] += size
+                    break
+            for idx, ext in enumerate(extList,start=0):
+                if row['filepath'].lower().endswith(ext):
+                  info_by_user[user][cntExtList[idx]] += 1
+                  info_by_user[user][sizeExtList[idx]] += size
        # except:
 #	  print(row['filepath'])
 
-    #print(info_by_user)
+# normalize
+    for user in info_by_user:
+        info_by_user[user]["TBfileSize"]='{size:.2f}'.format(size=info_by_user[user]["fileSize"]/1.1e12)
+	for idx, bin in enumerate(sizeBinNameList,start=0):
+            #info_by_user[user][TBsizeBinNameList[idx]]='{size:.2f}'.format(size=info_by_user[user][sizeBinNameList[idx]]/1.1e12)
+            info_by_user[user][TBsizeBinNameList[idx]]=TB_from_bytes(info_by_user[user][sizeBinNameList[idx]])
+	for idx, bin in enumerate(sizeExtList,start=0):
+            #info_by_user[user][TBsizeExtList[idx]]='{size:.2f}'.format(size=info_by_user[user][sizeExtList[idx]]/1.1e12)
+            info_by_user[user][TBsizeExtList[idx]]=TB_from_bytes(info_by_user[user][sizeExtList[idx]])
     
-    fieldnames = ["user","fileCnt","fileSize","Last access","Last modified"]
+    fieldnames = ["user","fileCnt","fileSize","TBfileSize","Last access","Last modified"]
     fieldnames.extend(cntBinNameList)
-    fieldnames.extend(sizeBinNameList)
+    fieldnames.extend(TBsizeBinNameList)
     fieldnames.extend(cntExtList)
+    fieldnames.extend(TBsizeExtList)
+    fieldnames.extend(sizeBinNameList)
     fieldnames.extend(sizeExtList)
     cga_util.dump_dict_table(outfile,info_by_user,fields=fieldnames,ragged_ok = True)
     #cga_util.dump_dict_table(outfile,info_by_user)
